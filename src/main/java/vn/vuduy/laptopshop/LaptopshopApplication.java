9e4cd51c65b0639653789540f7c727c80d90f578
VuDuy <vuduy24122004@gmail.com>
2025-05-22 17:45:31 +0700

    feat: disable security

--- src/main/java/vn/vuduy/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/vuduy/laptopshop/LaptopshopApplication.java
@@ -3,7 +3,11 @@ package vn.vuduy.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Disable Security(Bảo mật login)
+//include(Bao gồm)----exclude(Loại bỏ)
+//exclude = org.springframework.boot.autoconfigure.security.servlet(Config bình thường riêng class này loại bỏ ra)
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {

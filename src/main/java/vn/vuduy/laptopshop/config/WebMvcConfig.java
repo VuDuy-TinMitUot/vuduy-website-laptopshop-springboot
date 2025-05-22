3d148c7bb4f55ed9d547fde8692159977bffefd7
VuDuy <vuduy24122004@gmail.com>
2025-05-22 18:50:33 +0700

    chore: setup url anh xa file resources/css/

--- src/main/java/vn/vuduy/laptopshop/config/WebMvcConfig.java
+++ src/main/java/vn/vuduy/laptopshop/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -27,4 +28,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
     // ------> Hai annotation trên thay thế được cho Folder
     // static(application.properties).
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+    }
+    // bất kỳ yêu cầu nào đến URL bắt đầu bằng /css/ sẽ được ánh xạ đến các file CSS
+    // trong thư mục resources/css/ của dự án.
 }

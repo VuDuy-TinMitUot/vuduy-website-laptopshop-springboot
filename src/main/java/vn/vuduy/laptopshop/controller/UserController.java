6fc265a788c7eb97e5062ef423466827d850cc90
VuDuy <vuduy24122004@gmail.com>
2025-05-22 19:40:53 +0700

    feat: send data (submit form user)

--- src/main/java/vn/vuduy/laptopshop/controller/UserController.java
+++ src/main/java/vn/vuduy/laptopshop/controller/UserController.java
@@ -2,13 +2,24 @@ package vn.vuduy.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import vn.vuduy.laptopshop.domain.User;
 
 @Controller
 public class UserController {
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User vuduy) {
+        System.out.println("Run here" + vuduy);
+        return "hello";
     }
 }

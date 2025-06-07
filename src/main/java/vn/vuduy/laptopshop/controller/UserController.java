00844da832ca0f6921dacc373f54564d9f830963
VuDuy <vuduy24122004@gmail.com>
2025-06-07 22:57:45 +0700

    docs: note use send data(submit form user)

--- src/main/java/vn/vuduy/laptopshop/controller/UserController.java
+++ src/main/java/vn/vuduy/laptopshop/controller/UserController.java
@@ -13,10 +13,13 @@ public class UserController {
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
-        model.addAttribute("newUser", new User());
+        model.addAttribute("newUser", new User()); // new User(): khởi tạo Constructor không tham số VÀ gửi sang view để
+                                                   // hiện form(nhồi dữ liệu null vào "newUser")
+
         return "admin/user/create";
     }
 
+    // Nhồi dữ liệu từ các input HTML vào thuộc tính lớp User(@ModelAttribute)
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User vuduy) {
         System.out.println("Run here" + vuduy);
